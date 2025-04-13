2f7a76ddbdf2882506f0fa731c598dce7a3b9d63
DaLebedev <dalebedev@dmacc.edu>
2025-04-13 01:22:56 -0500

    Added Dynamic Star Rating & Shopping Cart
    Added ratings for items with dynamic star count. Added shopping cart and buttons to add items to cart. Applied styling and minor layout changes.

--- app/page.js
+++ app/page.js
@@ -1,11 +1,21 @@
 "use client";
 import { useState, useEffect } from "react";
+import { BookOpenIcon, ShoppingCartIcon } from '@heroicons/react/24/solid';
+import { Nunito } from 'next/font/google';
 import ItemCard from "@/components/ItemCard";
+
+const nunito = Nunito({
+  subsets: ['latin'],
+  weight: ['800'],
+});
 
 const Home = () => {
 
   // Var to store list of items
   const [items, setItems] = useState([]);
+  
+  // Var to represent cart
+  const [cart, setCart] = useState(0);
 
   // Get items from items.json once components are rendered
   useEffect(() => {
@@ -19,21 +29,40 @@ const Home = () => {
     getItemData();
   }, []);
 
-  return (
-      <div className="space-y-8 p-4">
-          <div className="space-y-6">
-          </div>
+  // Function to handle adding item to cart
+  const addToCart = () => {
+    setCart(cart + 1);
+    console.log(cart);
+  }
 
-          <div className="grid grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-4">
-            {items.map((item, index) => (
-              < ItemCard
-                item={item}
-                key={index}
-                src={item.img}
-              />
-            ))}
-          </div>
+  return (
+    // TODO: ADD SEARCH BAR
+    <div className="space-y-8 p-4">
+      <nav className={`p-4 flex text-white items-center justify-between shadow-md ${nunito.className}`} style={{ backgroundColor: "#00C763", borderRadius: "10px" }}>
+        {/* Logo and Title */}
+        <div className="flex text-3xl font-semibold items-center gap-x-2">
+          <BookOpenIcon className="w-10 h-10" />
+          MiniMart Bookstore
+        </div>
+        
+        {/* Cart Icon */}
+        <div className="flex items-center gap-x-2 border-2 border-white rounded-full py-1 px-3">
+          <ShoppingCartIcon className="w-7 h-7" />
+          <span>{cart}</span>
+        </div>
+      </nav>
+
+      <div className="grid grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-4">
+        {items.map((item, index) => (
+          < ItemCard
+          item={item}
+          key={index}
+          src={item.img}
+          addToCart={addToCart}
+          />
+        ))}
       </div>
+    </div>
   );
 };
 
--- components/ItemCard.js
+++ components/ItemCard.js
@@ -1,7 +1,28 @@
 import { Card, CardContent, CardDescription, CardHeader, CardTitle, } from "@/components/ui/card";
 import Image from "next/image";
+import { StarIcon } from '@heroicons/react/24/solid'; 
 
-const ItemCard = ({ item }) => {
+const ItemCard = ({ item, addToCart }) => {
+
+    const getStars = (rating) => {
+        let fullStars = Math.floor(rating);
+        let stars = [];
+
+        for (let i = 0; i < fullStars; i++) {
+            stars.push(
+                <StarIcon key={`full-${i}`} className="text-yellow-500 w-5 h-5" />
+            );
+        }
+
+        let remainder = (rating - fullStars) * 100;
+        if (remainder > 0) {
+            console.log(remainder);
+                stars.push(
+                    <StarIcon key={`halfStar}`} className="text-yellow-500 w-5 h-5" style={{ clipPath: `inset(0 ${100 - remainder}% 0 0)` }}/>
+            );
+        }
+        return stars;
+    }
 
     return (
         <Card>
@@ -14,12 +35,20 @@ const ItemCard = ({ item }) => {
                 />
             </CardContent>
             <CardHeader>
-                <div id="starRating">{item.rating}</div>
+                <div className="flex items-center justify-start gap-x-1"> 
+                    <span id="starRating" className="flex justify-content padding-1">{getStars(item.rating)}</span>
+                    <span className="text-sm text-muted-foreground">{item.rating}</span>
+                </div>
                 <CardTitle>{item.title}</CardTitle>
                 <CardDescription className={"flex justify-between items-center"}>
                     <span>{item.author}</span>
                     <span>${item.price}</span>
                 </CardDescription>
+
+                {/* // TODO: FINISH BUTTON TO BUY BOOK */}
+                <button onClick={addToCart} className="{`flex text-white py-2 font-semibold gap-x-2 ${nunito.className}`}" style={{ backgroundColor: "#00C763", borderRadius: "10px" }}>
+                    Add to Cart 
+                </button>
             </CardHeader>
         </Card>
     );
